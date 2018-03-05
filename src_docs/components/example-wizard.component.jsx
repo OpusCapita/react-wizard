86821f2a7890fd2d96dc160c2a191360c02d45ae
Boris Horosh <boris.horosh@opuscapita.com>
2018-03-05 22:10:37 +0200

    Fixed footer styles. Added demo check-boxes to an example page

--- src_docs/components/example-wizard.component.jsx
+++ src_docs/components/example-wizard.component.jsx
@@ -3,23 +3,60 @@
 import React from 'react';
 import Wizard from '../../src';
 
+import './example-wizard.scss';
+
 export default class ExampleWizard extends React.PureComponent {
   constructor(props) {
     super(props);
 
+    this.state = {
+      showSaved: false,
+      showPageIndicator: true,
+    };
+
     this.steps = [{
       id: '1',
       name: 'Page 1',
-      component: <div>My wizard page 1</div>,
+      component: this.getFirstPage(),
     }, {
       id: '2',
       name: 'Page 2',
-      component: <div>My wizard page 2</div>,
+      component: <h3>My wizard page 2</h3>,
     }, {
       id: '3',
       name: 'Page 3',
-      component: <div>My wizard page 3</div>,
+      component: <h3>My wizard page 3</h3>,
     }];
+  }
+
+  getFirstPage = () => (
+    <div id="wizard-page-1">
+      <h3>My wizard page 1</h3>
+      <div className="input-row">
+        <label htmlFor="show-save-successfully">
+          <input id="show-save-successfully" type="checkbox" onChange={this.changeHandlerForShowSaved} />
+          <span>Show element: Saved successfully</span>
+        </label>
+      </div>
+      <div className="input-row">
+        <label htmlFor="show-page-indicator">
+          <input id="show-page-indicator" type="checkbox" defaultChecked onChange={this.changeHandlerForPageIndicator} />
+          <span>Show page indicator</span>
+        </label>
+      </div>
+    </div>
+  );
+
+  changeHandlerForPageIndicator = (e) => {
+    this.setState({
+      showPageIndicator: e.target.checked,
+    });
+  }
+
+  changeHandlerForShowSaved = (e) => {
+    this.setState({
+      showSaved: e.target.checked,
+    });
   }
 
   saveWizard = () => {
@@ -37,6 +74,8 @@ export default class ExampleWizard extends React.PureComponent {
         cancel={this.cancelWizard}
         steps={this.steps}
         localizationTexts={{ save: 'Save', cancel: 'Cancel', saved: 'Saved successfully' }}
+        showSaveSuccess={this.state.showSaved}
+        showPageIndicator={this.state.showPageIndicator}
       />
     );
   }
