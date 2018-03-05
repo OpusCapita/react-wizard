061fba96be9e6094446050333372a1fe1a5ec5d4
Boris Horosh <boris.horosh@opuscapita.com>
2018-03-05 21:04:13 +0200

    Added Wizard component. Removed dependency opuscapita/react-icons, doesn't work. Added and used react-icons library

---
+++ src/components/wizard-footer.component.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
+
+import FaCaretLeft from 'react-icons/lib/fa/caret-left';
+import FaCaretRight from 'react-icons/lib/fa/caret-right';
+import FaCheck from 'react-icons/lib/fa/check';
+
+import './wizard-footer.component.scss';
+
+
+export default class WizardFooter extends React.PureComponent {
+  static propTypes = {
+    save: PropTypes.func.isRequired,
+    disableSave: PropTypes.bool,
+    cancel: PropTypes.func.isRequired,
+    disableCancel: PropTypes.bool,
+    localizationTexts: PropTypes.shape({
+      save: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+      cancel: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+    }).isRequired,
+    steps: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
+      component: PropTypes.node.isRequired,
+    })).isRequired,
+    selectPage: PropTypes.func.isRequired,
+    currentStep: PropTypes.number.isRequired,
+    showPageIndicator: PropTypes.bool,
+    showSaveSuccess: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    showPageIndicator: true,
+    disableSave: false,
+    disableCancel: false,
+    showSaveSuccess: false,
+  };
+
+  getIndicators = () => (
+    this.props.steps.map((step, i) => {
+      if (i === this.props.currentStep) {
+        return <span key={step.id} className="tab-indicator tab-highlight" />;
+      }
+      return <span key={step.id} className="tab-indicator" />;
+    })
+  );
+
+  render() {
+    const {
+      currentStep, localizationTexts, showPageIndicator, showSaveSuccess,
+      steps,
+    } = this.props;
+    return (
+      <div id="wizard-footer">
+        <section className="left column" />
+        <section className="center column">
+          {showPageIndicator &&
+          <div id="bottom-indicators">
+            {this.getIndicators()}
+          </div>
+          }
+          <div id="bottom-buttons">
+            <section>
+              <Button
+                disabled={currentStep === 0}
+                id="previous-step"
+                onClick={event => this.props.selectPage(event, currentStep - 1)}
+              >
+                <FaCaretLeft />
+              </Button>
+            </section>
+            <section>
+              <Button
+                id="save-button"
+                onClick={this.props.save}
+                disabled={this.props.disableSave}
+              >
+                {localizationTexts.save}
+              </Button>
+
+              <Button
+                id="cancel-button"
+                onClick={this.props.cancel}
+                disabled={this.props.disableCancel}
+              >
+                {localizationTexts.cancel}
+              </Button>
+            </section>
+            <section>
+              <Button
+                disabled={currentStep === steps.length - 1}
+                id="next-step"
+                onClick={event => this.props.selectPage(event, currentStep + 1)}
+              >
+                <FaCaretRight />
+              </Button>
+            </section>
+          </div>
+        </section>
+        <section className="right column">
+          {showSaveSuccess &&
+            <div className="save-success-container">
+              <FaCheck className="save-success-indicator" />
+              {localizationTexts.saved}
+            </div>}
+        </section>
+      </div>
+    );
+  }
+}
--- src_docs/components/example-wizard.component.jsx
+++ src_docs/components/example-wizard.component.jsx
@@ -1,8 +1,44 @@
+/* eslint-disable no-console  */
+
 import React from 'react';
 import Wizard from '../../src';
 
 export default class ExampleWizard extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.steps = [{
+      id: '1',
+      name: 'Page 1',
+      component: <div>My wizard page 1</div>,
+    }, {
+      id: '2',
+      name: 'Page 2',
+      component: <div>My wizard page 2</div>,
+    }, {
+      id: '3',
+      name: 'Page 3',
+      component: <div>My wizard page 3</div>,
+    }];
+  }
+
+  saveWizard = () => {
+    console.log('Pressed Save button');
+  }
+
+  cancelWizard = () => {
+    console.log('Pressed Cancel button');
+  }
+
   render() {
-    return <Wizard />;
+    return (
+      <Wizard
+        save={this.saveWizard}
+        cancel={this.cancelWizard}
+        steps={this.steps}
+        localizationTexts={{ save: 'Save', cancel: 'Cancel', saved: 'Saved successfully' }}
+        showSaveSuccess
+      />
+    );
   }
 }
